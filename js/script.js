98fa84a1e87bfd0b122b60dec65f5f4ca6279bdf
Nasya <joeaqeem@gmail.com>
2025-10-04 01:45:57 +0700

    Update content

--- js/script.js
+++ js/script.js
@@ -5,36 +5,46 @@ let todos = [];
 function addTodo() {
     const todoInput = document.getElementById("todo-inputs");
     const todoDate = document.getElementById("todo-date");
+
+    /// Validate input
     if (validateInput (todoInput.value, todoDate.value)) {
         let todo = {task: todoInput.value, date :todoDate.value };
         todos.push(todo);
-        console.log(todos);
-
+        
+        ///Render the updated todo list
+        renderTodo();
     }
 }
  
 function renderTodo () {
-    const todoList = document.getElementsById ("todo-list");
-    todoList.innerHTML = '';
+    const todoList = document.getElementsById("todo-list");
+    let hmlElements = '';
 
     todos.forEach((todo, index) => {
-        todoList.innerHTML += <li class="border p-2 mb-2 flex justify-between items-center">
-            <div>
-                <p class="font-bold">${todo.task}</p>
-                <p class="text-sm text-gray-500">${todo.date}</p>
-            </div>
+       htmlElements += `
+            '<li class="border p-2 mb-2 flex justify-between items-center'
+                <div>
+                    <><p> class="font-bold"&gt;${todo.task}</p><p class="text-sm text-gray-500">${todo.date}</p></></div>
             <button onclick="deleteTodo(${index})" class="bg-red-500 text-white p-1 rounded">Delete</button>
-        </li>;
+        </li>\
+    `;
     });
 }
-function deleteAllTodo () {}
 
-function filterTodo () {}
+function deleteAlltodo (){
+    /// Clear the todos array
+    todos = [];
+
+    // Render the empty todo list
+    renderTodo();
+}
 function validateInput (todo,date) {
     if (!todo ==='' || !date ==='') {
+        /// Show an alert if validation falls
         alert("Please fill in all fields");
         return  false;
     }
-        return false;
+    /// Input is valid 
+        return true;
 
 }
