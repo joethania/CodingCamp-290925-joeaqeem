67fb7531a9b85f4b9aac7769d74e730a219a0bdb
Nasya <you@example.com>
2025-10-03 11:54:27 +0700

    implement todo app structur with HTML,CSS,and JavaScript functionally

---
+++ js/script.js
@@ -0,0 +1,40 @@
+console.log("Hello World");
+
+let todos = [];
+
+function addTodo() {
+    const todoInput = document.getElementById("todo-inputs");
+    const todoDate = document.getElementById("todo-date");
+    if (validateInput (todoInput.value, todoDate.value)) {
+        let todo = {task: todoInput.value, date :todoDate.value };
+        todos.push(todo);
+        console.log(todos);
+
+    }
+}
+ 
+function renderTodo () {
+    const todoList = document.getElementsById ("todo-list");
+    todoList.innerHTML = '';
+
+    todos.forEach((todo, index) => {
+        todoList.innerHTML += <li class="border p-2 mb-2 flex justify-between items-center">
+            <div>
+                <p class="font-bold">${todo.task}</p>
+                <p class="text-sm text-gray-500">${todo.date}</p>
+            </div>
+            <button onclick="deleteTodo(${index})" class="bg-red-500 text-white p-1 rounded">Delete</button>
+        </li>;
+    });
+}
+function deleteAllTodo () {}
+
+function filterTodo () {}
+function validateInput (todo,date) {
+    if (!todo ==='' || !date ==='') {
+        alert("Please fill in all fields");
+        return  false;
+    }
+        return false;
+
+}
